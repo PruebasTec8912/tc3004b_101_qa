b63c9a63c5d58640ddc540013d0a0583bd3638bf
Gabo8912 <128503197+Gabo8912@users.noreply.github.com>
2025-02-24 11:10:01 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-function add(){
+ function calculate(){
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
-
     let result;
     result = Number(num1) + Number(num2);
 
-    document.getElementById("result").innerText= "Result: " + result;
+    document.getElementById("result").innerText = "result: " + result;
 }
+function add(){
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    let result;
+    result = Number(num1) + Number(num2);
 
+    document.getElementById("result").innerText = "result: " + result;
+}
 function sub(){
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
-
     let result;
     result = Number(num1) - Number(num2);
 
-    document.getElementById("result").innerText= "Result: " + result;
+    document.getElementById("result").innerText = "result: " + result;
 }
-
-
-function reset(){
+function resetFields(){
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
-    document.getElementById("result").innerText = "Result:";
-}
+    document.getElementById("operator").value = "+";
+    document.getElementById("result").value = "result: ";
+} 
