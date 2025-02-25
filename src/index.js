3d3db3cf0604acebb000e0a454caa0eaacb31b27
Gabo8912 <128503197+Gabo8912@users.noreply.github.com>
2025-02-24 21:26:59 -0600

    a

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
- function calculate(){
-    const num1 = document.getElementById("num1").value;
-    const num2 = document.getElementById("num2").value;
-    let result;
-    result = Number(num1) + Number(num2);
-
-    document.getElementById("result").innerText = "result: " + result;
-}
 function add(){
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
@@ -14,7 +6,8 @@ function add(){
 
     document.getElementById("result").innerText = "result: " + result;
 }
-function sub(){
+
+function subtract(){
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
     let result;
@@ -22,9 +15,12 @@ function sub(){
 
     document.getElementById("result").innerText = "result: " + result;
 }
+
+
+
 function resetFields(){
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
     document.getElementById("operator").value = "+";
     document.getElementById("result").value = "result: ";
-} 
+}
